ef5747fd534ab376c5e05b0b5555037766a2eb14
beatriz-nagae <138304130+beatriz-nagae@users.noreply.github.com>
2025-04-01 23:06:19 -0300

    Update mongodb.js - melhorias
    saber O QUE ta rolando e em que fase da conexão estamos

--- saborvete-backend/routes/mongodb.js
+++ saborvete-backend/routes/mongodb.js
@@ -1,21 +1,38 @@
 //(admin:saborvete) (senha: reinilton)
-const mongodb = require('mongodb').MongoClient
-const url = "mongodb+srv://saborvete:reinilton@cluster0.znnugwe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const { MongoClient } = require('mongodb'); 
 
-mongodb.connect(url,(erro,banco)=> {
-    if(erro)throw erro;
-    const dbo=banco.db("Saborvete")
-    const obj = [
-        { nome: "Picolé de Morango", preco: 8, quantidade: 20 },
-        { nome: "Pote de Morango", preco: 20, quantidade: 20 },
-        { nome: "Picolé de Chocolate", preco: 8, quantidade: 20 },
-        { nome: "Pote de Chocolate", preco: 20, quantidade: 20 },
-        { nome: "Picolé de Uva", preco: 8, quantidade: 20 },
-        { nome: "Pote de Uva", preco: 20, quantidade: 20 },
-        { nome: "Picolé de Coco", preco: 8, quantidade: 20 },
-        { nome: "Pote de Coco", preco: 20, quantidade: 20 },
-        { nome: "Picolé de Milho", preco: 8, quantidade: 20 },
-        { nome: "Pote de Milho", preco: 20, quantidade: 20 }
-    ]
-    //a ser continuado............ado a ado cada um no seu quadrado
-})
+const url = "mongodb+srv://saborvete:reinilton@cluster0.znnugwe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+async function conectarBanco() {
+    const cliente = new MongoClient(url);
+    
+    try {
+        await cliente.connect();
+        console.log("Ai que sucesso! Conectou ao Mongodb!!");
+
+        const dbo = cliente.db("Saborvete");
+        const obj = [
+            { nome: "Picolé de Morango", preco: 8, quantidade: 20 },
+            { nome: "Pote de Morango", preco: 20, quantidade: 20 },
+            { nome: "Picolé de Chocolate", preco: 8, quantidade: 20 },
+            { nome: "Pote de Chocolate", preco: 20, quantidade: 20 },
+            { nome: "Picolé de Uva", preco: 8, quantidade: 20 },
+            { nome: "Pote de Uva", preco: 20, quantidade: 20 },
+            { nome: "Picolé de Coco", preco: 8, quantidade: 20 },
+            { nome: "Pote de Coco", preco: 20, quantidade: 20 },
+            { nome: "Picolé de Milho", preco: 8, quantidade: 20 },
+            { nome: "Pote de Milho", preco: 20, quantidade: 20 }
+        ];
+
+        const resultado = await dbo.collection("sorvetes").insertMany(obj);
+        console.log(`🎉 ${resultado.insertedCount} Tamo Inserindo Tamo Inserindo! Sorvetes iNSERIDOS`);
+    
+    } catch (erro) {
+        console.error("Deu ruim ao conectar ou inserir os dados:", erro);
+    
+    } finally {
+        await cliente.close();
+        console.log("Conexão com MongoDB CLOOOOSED.");
+    }
+}
+conectarBanco();
